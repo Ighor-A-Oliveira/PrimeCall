727adaacdfbdb3345ded6fb1f4d55e1d0de175d2
Ighor Oliveira <ighor9991@gmail.com>
2024-08-18 13:46:34 -0300

    improved the page not found component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,12 @@
-
+import { useEffect } from "react";
 
 export default function NotFound() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <p className='py-96 bg-fuchsia-400 text-white text-center font-jura text-4xl'>Erro Pagina não encontrada</p>
-  )
+    <p className="py-96 bg-fuchsia-400 text-white text-center font-jura text-4xl">
+      Erro Pagina não encontrada
+    </p>
+  );
 }
